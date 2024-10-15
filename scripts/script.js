9ce265e7d32e1e0e712564c4562ad2530f35e53f
Derek Hayes <derek2099@gmail.com>
2024-10-14 20:01:34 -0500

    Change x and o to svgs

--- scripts/script.js
+++ scripts/script.js
@@ -176,10 +176,10 @@ const displayController = (() => {
         const b = gameBoard.getBoard();
         for (let i = 0; i < b.length; i++) {
             if (b[i] === 1) {
-                cells[i].textContent = 'X';
+                cells[i].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="190px" viewBox="0 -960 960 960" width="190px" fill="#FB8500"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>';
             }
             else if (b[i] === 2) {
-                cells[i].textContent = 'O';
+                cells[i].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="150px" viewBox="0 -960 960 960" width="150px" fill="#219EBC"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
             }
             else {
                 cells[i].textContent = '';
